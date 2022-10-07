527025af66bc3dcffdbe1e345eb7963bcd4830ca
grilled-fish <115198941+grilled-fish@users.noreply.github.com>
2022-10-07 09:27:06 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("2f34yushouyuan")
+    basic.showString("1")
+    basic.showString("2")
+    basic.showString("3")
+    basic.showString("4")
+    basic.showString("5")
+    basic.showString("6")
+})
